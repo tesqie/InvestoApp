02a67855259a05242f177d47dacd16e1162f1240
tesqie <tesqie@protonmail.com>
2017-03-09 14:57:38 -0500

    changed symantics
    mk,nkl

--- src/investoapp/Account.java
+++ src/investoapp/Account.java
@@ -5,6 +5,24 @@ package investoapp;
  * @author tesqie
  */
 public class Account {
-    private String username;
-    private String password;
+    private final String username;
+    private final String password;
+    private final String email;
+
+    public Account(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+    
 }
